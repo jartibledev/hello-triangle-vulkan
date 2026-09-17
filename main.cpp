86bccb99dc4d4cc6ac5c54901ba38a90d33776b2
Jartible <jartibledev@proton.me>
2026-09-17 22:52:22 +0200

    feat(instance): add createInstance() function and fill the appInfo struct

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ public:
 }
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
 	void initWindow() {
 		glfwInit();
@@ -29,8 +30,18 @@ private:
 		
 	}
 	void initVulkan() {
-
+		createInstance();
 	}
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+	}
+
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
