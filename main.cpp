a691d69fe316b89cb7b49b99c7f773da4a8c86b1
Jartible <jartibledev@proton.me>
2026-09-17 22:34:52 +0200

    feat(glfw): add initWindows() functionm mainLoop() functon and WIDTH and HEIGHT constants

--- main.cpp
+++ main.cpp
@@ -7,19 +7,34 @@
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 1240;
+const uint32_t HEIGHT = 720;
+
 class HelloTriangleApplication {
 public: 
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 }
 private:
+	GLFWwindow* window;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		
+	}
 	void initVulkan() {
 
 	}
 	void mainLoop() {
-
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 	void cleanup() {
 
