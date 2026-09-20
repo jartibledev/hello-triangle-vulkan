2d8af522b0aa6508f823322187df94ec2fc9d7e4
Jartible <jartibledev@proton.me>
2026-09-20 03:53:35 +0200

    feat(VkDebugUtilsMessengerCreateInfoEXT createInfo{}): add struct createInfo and fix a error of sintaxis
    I forgot to write the void* pUserData in debugCallback function. So when I filled the struct, the parameter createInfo.pfnUserCallback = debugCallback send a compiler error (C2440), that means the compiler cannot convert implicity the value because an argument is missing.

--- main.cpp
+++ main.cpp
@@ -53,6 +53,18 @@ private:
 	void setupDebugMessenger() {
 		if (!enableValidationLayers) return;
 
+		VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+			VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = debugCallback;
+		createInfo.pUserData = nullptr;
+
+
 	}
 	void createInstance() {
 		if (enableValidationLayers && !checkValidationLayerSupport()) {
@@ -146,13 +158,15 @@ private:
 		return extensions;
 	}
 
-	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallBack(
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 		VkDebugUtilsMessageTypeFlagsEXT messageType,
-		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData) {
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData) {
 
 		std::cerr << "validation layer: " << pCallbackData
 			-> pMessage << std::endl;
+
 
 		return VK_FALSE;
 	}
