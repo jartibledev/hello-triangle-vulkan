123f7ca4d16a3efd8215cd53c3435e1faf6e04c9
Jartible <jartibledev@proton.me>
2026-09-20 04:52:14 +0200

    feat(proxy): add CreateDebugUtilsMessengerEXT function.

--- main.cpp
+++ main.cpp
@@ -23,6 +23,18 @@ const std::vector < const char*> validationLayers = {
 	const bool enableValidationLayers = true;
 #endif
 
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+		const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator,
+		VkDebugUtilsMessengerEXT* pDebugMessenger) {
+		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+		}
+		else {
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+		}
+}
 
 class HelloTriangleApplication {
 public: 
